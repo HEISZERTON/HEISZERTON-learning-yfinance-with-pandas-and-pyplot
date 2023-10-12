ccf418ad79d028fdded74a41bf36b52788616728
Emmitt Heisz <emmitt.heisz@gmail.com>
2023-10-11 21:07:59 -0400

    part 2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import pandas as pd 
+# Folwing youtube tutorial 
+# https://www.youtube.com/watch?v=9nB__kJio-M
+
+import pandas
 import matplotlib.pyplot as plt
 from datetime import datetime
 plt.style.use('seaborn-v0_8')
@@ -8,15 +11,31 @@ import yfinance as yf
 def main():
 	msft = yf.Ticker('msft')
 
-	# stockinfo = msft.info
+	stockinfo = msft.info
 
-	# for key,value in stockinfo.items():
-	# 	print(key, ":", value)
+	for key,value in stockinfo.items():
+		print(key, ":", value)
 
-	# numshares = msft.info['sharesOutstanding']
-	# print(numshares)
+	numshares = msft.info['sharesOutstanding']
+	print(numshares)
+	print(msft.balancesheet)
 
-	df = msft.dividends
+def plot_chart(stock_ticker):
+	security = yf.Ticker(stock_ticker)
+
+	today = datetime.now().date().strftime("%Y-%m-%d")
+	df = security.history(start = '2020-01-01', end = today)
+	plt.figure()
+	plt.plot(df['Close'])
+	plt.title(security.info['shortName'] + ' Price History')
+	plt.ylabel('Price ($)')
+	plt.xlabel('Year')
+	plt.show()
+
+def plot_dividend(stock_ticker):
+	security = yf.Ticker(stock_ticker)
+
+	df = security.dividends
 	data = df.resample('Y').sum()
 	data = data.reset_index()
 	data['Year'] = data['Date'].dt.year
@@ -25,8 +44,27 @@ def main():
 	plt.bar(data['Year'],data['Dividends'])
 	plt.ylabel('Dividend yeild ($)')
 	plt.xlabel('Year')
-	plt.title('Microsoft Dividend History')
-	plt.xlim(2002,2023)
+	plt.title(security.info['shortName'] + ' Dividend History')
+	plt.xlim(2002,2020)
 	plt.show()
 
-main()
+def chart_list(ticker_list):
+
+	df = pandas.DataFrame()
+	today = datetime.now().date().strftime("%Y-%m-%d")
+
+	for security in ticker_list:	
+		df[security] = yf.Ticker(security).history(start = '2020-01-01', end = today).Close
+
+	plt.figure()
+	plt.plot(df)
+	plt.legend(ticker_list)
+	plt.title('Security Price History')
+	plt.ylabel('Price ($)')
+	plt.xlabel('Year')
+	plt.show()
+
+#plot_dividend('msft')
+#plot_chart('msft')
+chart_list(['voo','msft','aapl','nvda','jnj','axp','dis'])
+#main()
